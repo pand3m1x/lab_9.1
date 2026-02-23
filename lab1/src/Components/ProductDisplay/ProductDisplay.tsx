803093859db8ad1a6b883c6200b5efdb7aae288d
TheRedQueen <pand3m1x@gmail.com>
2026-02-22 19:07:58 -0500

    ⋆꙳•̩̩͙❅*̩̩͙‧͙ Merged the work from class group with what I did, some react errors, and want to reconstruct the return statements  ‧͙*̩̩͙❆ ͙͛ ˚₊⋆

--- lab1/src/Components/ProductDisplay/ProductDisplay.tsx
+++ lab1/src/Components/ProductDisplay/ProductDisplay.tsx
@@ -1,9 +1,27 @@
-import interface Product from './'
-import interface ProductDisplayProps from './'
+// import interface Product from './'
+import type { ProductDisplayProps } from "../../types";
 
-function ProductDisplay(){
-
-  return();
+function ProductDisplay({ product, showDescription, showStockStatus, onAddToCart, children}:ProductDisplayProps){
+    return (
+       <div>
+        
+       </div>
+    )
 }
 
-export default ProductDisplay
+export default ProductDisplay;
+
+//  <div className="flex flex-row gap-3.5 border border-gray-300 m-3.5">
+//             {product.imageUrl && <div><img src={product.imageUrl} /></div>}
+//             <div className="">
+//                 <p>Product ID: {product.id}</p>
+//                 <p>Product Name: {product.name}</p>
+//                 <p>Price: ${product.price}</p>
+//                 {showDescription && (<p>Description: {product.description}</p>)}
+//                 {showStockStatus && (<p>Stock Status: {product.inStock==true ? "In Stock" : "Out of Stock"}</p>)}
+//                 {onAddToCart && (<button onClick={()=>onAddToCart(product.id)} className="border border-gray-300 p-3.5 pt-1.5 pb-1.5">Add to Cart</button>)}
+//                 <div>
+//                     {children}
+//                 </div>
+//             </div>
+//         </div>
--- lab1/src/Components/UserProfileCard/UserProfileCard.tsx
+++ lab1/src/Components/UserProfileCard/UserProfileCard.tsx
@@ -1,9 +1,28 @@
-import interface User from './'
-import interface UserProfileCardProps from './'
+// import interface User from './'
+import type { UserProfileCardProps } from "../../types";
 
-function UserProfileCard(){
+function UserProfileCard({user , showEmail, showRole, onEdit, children }:UserProfileCardProps){
 
-  return();
+    
+    return (
+     
+    );
 }
 
-export default UserProfileCard
+export default UserProfileCard;
+
+  //  <>
+  //           <div className="flex flex-row gap-3.5 border border-gray-300 m-3.5">
+  //               {user.avatarUrl && <div><img src={user.avatarUrl} /></div>}
+  //               <div className="">
+  //                   <p>User ID: {user.id}</p>
+  //                   <p>User Name: {user.name}</p>
+  //                   {showEmail && (<p>Email: {user.email}</p>)}
+  //                   {showRole && (<p>Role: {user.role}</p>)}
+  //                   {onEdit && (<button onClick={()=>onEdit(user.id)} className="border border-gray-300 p-3.5 pt-1.5 pb-1.5">Edit</button>)}
+  //                   <div>
+  //                       {children}
+  //                   </div>
+  //               </div>
+  //           </div>
+  //       </>
